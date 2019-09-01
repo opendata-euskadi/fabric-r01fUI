b928d34c2c5190c26d61e7413a5b471e7a8c5c4b
PCI <pci@ejie.eus>
2019-09-01 12:49:17 +0100

    Added methods to VaadinViewID to build it from the URL

--- r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/view/VaadinViewID.java
+++ r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/view/VaadinViewID.java
@@ -1,6 +1,12 @@
 package r01f.ui.vaadin.view;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+
 import r01f.guids.OIDBaseMutable;
+import r01f.types.url.Url;
+import r01f.util.types.StringSplitter;
+import r01f.util.types.Strings;
 
 /**
  * A view key
@@ -21,8 +27,33 @@ public class VaadinViewID
 	public static VaadinViewID valueOf(final String id) {
 		return VaadinViewID.forId(id);
 	}
+	public static VaadinViewID from(final Url url) {
+		return VaadinViewID.fromUrlFragment(url.getUrlPathFragment());
+	}
+	public static VaadinViewID fromUrlFragment(final String urlPathFragment) {
+		if (Strings.isNullOrEmpty(urlPathFragment)) throw new IllegalArgumentException("illegal url path fragment!!");
+		// vaadin url path fragment is like: #!{viewId}/{param}={value}/{param}={value}...
+		// extract the viewId
+		String withoutExclamation = urlPathFragment.indexOf("!") == 0 ? urlPathFragment.substring(1)
+																	  : urlPathFragment;
+		String viewId = Iterables.getFirst(StringSplitter.using(Splitter.on("/")
+																		.omitEmptyStrings()
+																		.trimResults())
+														 .at(withoutExclamation)
+														 .split(),
+										   null);
+		return VaadinViewID.forId(viewId);
+	}
 	@Override
 	public boolean is(final String id) {
 		return this.asString().equals(id);
 	}
+	public boolean is(final Url url) {
+		String urlPathFragment = url.getUrlPathFragment();
+		return this.isUrlPathFragment(urlPathFragment);
+	}
+	public boolean isUrlPathFragment(final String urlPathFragment) {
+		VaadinViewID viewId = VaadinViewID.fromUrlFragment(urlPathFragment);
+		return this.is(viewId);
+	}	
 }

ead2361a3e059831ea379f1f4bfe3bde9a64dbd3
PCI <pci@ejie.eus>
2019-10-17 22:16:57 +0200

    Added equals & hashcode to VaadinViewLanguageText

--- r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/view/VaadinViewLanguageText.java
+++ r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/view/VaadinViewLanguageText.java
@@ -1,5 +1,7 @@
 package r01f.ui.vaadin.view;
 
+import com.google.common.base.Objects;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,4 +21,25 @@ public class VaadinViewLanguageText
 /////////////////////////////////////////////////////////////////////////////////////////
     @Getter @Setter private Language _language;
     @Getter @Setter private String _text;
+/////////////////////////////////////////////////////////////////////////////////////////
+//	EQUALS                                                                          
+/////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+	public boolean equals(final Object other) {
+    	if (other == null) return false;
+    	if (this == other) return true;
+    	if (!(other instanceof VaadinViewLanguageText)) return false;
+    	VaadinViewLanguageText otherLangText = (VaadinViewLanguageText)other;
+    	boolean langEqs = _language != null ? _language.equals(otherLangText.getLanguage())
+    										: _language == null && otherLangText.getLanguage() == null ? true
+    																			  		 			   : false;
+    	boolean textEqs = _text != null ? _text.equals(otherLangText.getText())
+    									: _text == null && otherLangText.getText() == null ? true
+    																					   : false;
+    	return langEqs && textEqs;
+    }
+    @Override
+	public int hashCode() {
+    	return Objects.hashCode(_language,_text);
+    }
 }

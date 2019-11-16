1aee3b6384d691da1c2d94bbdb015f2ed9fbdedd
PCI <pci@ejie.eus>
2019-11-16 12:24:28 +0100

    UIViewObjectBase deprecated

--- r01fUIClasses/src/main/java/r01f/ui/viewobject/UIViewObjectWrappedBase.java
+++ r01fUIClasses/src/main/java/r01f/ui/viewobject/UIViewObjectWrappedBase.java
@@ -4,10 +4,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
-@Deprecated // use UIViewObjectWrappedBase
 @Accessors(prefix="_")
 @RequiredArgsConstructor
-public abstract class UIViewObjectBase<T> 
+public abstract class UIViewObjectWrappedBase<T> 
            implements UIViewObjectWrapped<T> {
 
 	private static final long serialVersionUID = 5054336695644200270L;

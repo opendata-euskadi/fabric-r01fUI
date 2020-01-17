f3018f635a75e322f101c03d6c703142f1b5623c
eaguirresarobe <eaguirresarobe@euro-help.es>
2020-01-17 13:42:58 +0100

    VaadinDateTimeRangeComponent - hour range selector changes

--- r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01ui/base/components/datetime/VaadinDateTimeRangeComponent.java
+++ r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01ui/base/components/datetime/VaadinDateTimeRangeComponent.java
@@ -13,30 +13,23 @@ import com.vaadin.ui.DateTimeField;
  * 		+++++++++++++++++++++++++++++++++++++++
  * </pre>
  */
-public class VaadinDateTimeRangeComponent 
+public class VaadinDateTimeRangeComponent
 	 extends VaadinDateTimeRangeComponentBase<LocalDateTime,DateTimeResolution,
 	 										  DateTimeField> {
 
 	private static final long serialVersionUID = -4908712937810703202L;
 /////////////////////////////////////////////////////////////////////////////////////////
-//	                                                                          
+//
 /////////////////////////////////////////////////////////////////////////////////////////
 	public VaadinDateTimeRangeComponent() {
 		super(DateTimeField::new);	// component factory
-		
+
 		// BEWARE!! add these styles to the app stylesheet
-		//	    .datetimepicker-time-only .v-inline-datefield-calendarpanel-header,
-		//	    .datetimepicker-time-only .v-inline-datefield-calendarpanel-body {
-		//	        display: none;
-		//	    }
-		//	    .datetimepicker-time-only .v-datefield-calendarpanel-header,
-		//	    .datetimepicker-time-only .v-datefield-calendarpanel-body {
-		//	        display: none;
-		//	    }
 		_dateUperBound.addStyleName("datetimepicker-time-only");
 		_dateLowerBound.addStyleName("datetimepicker-time-only");
-		
+
 		_dateUperBound.setDateFormat("HH:mm");
 		_dateLowerBound.setDateFormat("HH:mm");
+
 	}
 }

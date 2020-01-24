c51b88d8624bc05b41c4a4f1eb2656cefe5c6010
Isra <isra@r01.eus>
2020-01-24 09:22:56 +0100

    updated from master

--- r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/view/VaadinViews.java
+++ r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/view/VaadinViews.java
@@ -6,7 +6,6 @@ import java.math.BigInteger;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.Map;
 
 import com.vaadin.data.Binder;
@@ -24,7 +23,6 @@ import com.vaadin.data.converter.StringToFloatConverter;
 import com.vaadin.data.converter.StringToIntegerConverter;
 import com.vaadin.data.converter.StringToLongConverter;
 import com.vaadin.data.util.BeanUtil;
-import com.vaadin.navigator.Navigator;
 import com.vaadin.ui.AbstractDateField;
 import com.vaadin.ui.AbstractField;
 import com.vaadin.ui.AbstractLocalDateField;
@@ -68,53 +66,13 @@ public abstract class VaadinViews {
 	public static String vaadinViewUrlPathParamOf(final VaadinViewID viewId,final Map<String,String> navParams) {
 		return VaadinViews.vaadinViewUrlPathFragmentOf(viewId,navParams);
 	}
-	/**
-	 * Creates a Vaadin {@link Navigator} url params from the view id and params
-	 * @param viewId
-	 * @param navParams
-	 * @return
-	 */
+	@Deprecated 	// moved to VaadinNavigator
 	public static String vaadinViewUrlPathFragmentOf(final VaadinViewID viewId,final Map<String,String> navParams) {
-		String outUrlPathParam = null;
-		if (CollectionUtils.hasData(navParams)) {
-			StringBuilder paramsStr = new StringBuilder();
-			paramsStr.append(viewId)
-					 .append("/");
-			for (Iterator<Map.Entry<String,String>> meIt = navParams.entrySet().iterator(); meIt.hasNext(); ) {
-				Map.Entry<String,String> me = meIt.next();
-				paramsStr.append(me.getKey())
-				   		 .append("=")
-						 .append(me.getValue());
-				if (meIt.hasNext()) paramsStr.append("/");
-			}
-			outUrlPathParam = paramsStr.toString();
-		} else {
-			outUrlPathParam = viewId.asString();
-		}
-		return outUrlPathParam;
+		return VaadinNavigator.vaadinViewUrlPathFragmentOf(viewId,navParams);
 	}
-	/**
-	 * Creates a Vaadin {@link Navigator} url params from the view id and params
-	 * @param viewId
-	 * @param navParams
-	 * @return
-	 */
+	@Deprecated 	// moved to VaadinNavigator
 	public static String vaadinViewUrlPathParamOf(final VaadinViewID viewId,final Collection<String> navParams) {
-		String outUrlPathParam = null;
-		if (CollectionUtils.hasData(navParams)) {
-			StringBuilder paramsStr = new StringBuilder();
-			paramsStr.append(viewId)
-					 .append("/");
-			for (Iterator<String> meIt = navParams.iterator(); meIt.hasNext(); ) {
-				String param = meIt.next();
-				paramsStr.append(param);
-				if (meIt.hasNext()) paramsStr.append("/");
-			}
-			outUrlPathParam = paramsStr.toString();
-		} else {
-			outUrlPathParam = viewId.asString();
-		}
-		return outUrlPathParam;
+		return VaadinNavigator.vaadinViewUrlPathParamOf(viewId,navParams);
 	}
 /////////////////////////////////////////////////////////////////////////////////////////
 //	BINDING

78e40a4c26ae6ed3f6425e08498d271963481091
PCI <pci@ejie.eus>
2020-03-15 10:01:21 +0100

    VaadinViewI18NMessagesCanBeUpdated default method avoids the mandatory implementation of updateI18NMessages method

--- r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/view/VaadinViewI18NMessagesCanBeUpdated.java
+++ r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/view/VaadinViewI18NMessagesCanBeUpdated.java
@@ -3,5 +3,7 @@ package r01f.ui.vaadin.view;
 import r01f.ui.i18n.UII18NService;
 
 public interface VaadinViewI18NMessagesCanBeUpdated {
-	 void updateI18NMessages(final UII18NService i18n);
+	 default void updateI18NMessages(final UII18NService i18n) {
+		 // just do nothing
+	 }
 }

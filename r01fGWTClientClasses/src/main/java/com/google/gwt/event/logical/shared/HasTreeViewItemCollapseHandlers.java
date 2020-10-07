96c584e7aca8eab053b9cc19c2aa7aafb389abb1
jgonzacr <jgonzacr@CO01759T>
2020-10-07 09:59:37 +0200

    pom.xml > VARIABLES

--- r01fGWTClientClasses/src/main/java/com/google/gwt/event/logical/shared/HasTreeViewItemCollapseHandlers.java
+++ r01fGWTClientClasses/src/main/java/com/google/gwt/event/logical/shared/HasTreeViewItemCollapseHandlers.java
@@ -1,4 +1,4 @@
-package com.google.gwt.event.logical.shared;
+package com.google.gwt.event.logical.shared; 
 
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.event.shared.HasHandlers;

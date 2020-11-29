4a8da535249530784e02a5f25b9633487dfb7c38
pci <pci@ejie.eus>
2020-11-29 22:48:10 +0100

    Vaadin Validators

--- r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/VaadinValidators.java
+++ r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/VaadinValidators.java
@@ -7,6 +7,8 @@ import com.vaadin.data.ValueContext;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import r01f.types.contact.NIFValidator;
+import r01f.types.contact.PersonID;
 import r01f.ui.i18n.UII18NService;
 import r01f.util.types.Strings;
 
@@ -30,6 +32,23 @@ public abstract class VaadinValidators {
 
 			// beware!! this is called for every keystroke on the text field
 			return ValidationResult.ok();
+		}
+	}
+	@RequiredArgsConstructor
+	public static class VaadinSpanishNIFValidator
+	   	 	 implements Validator<PersonID> {
+
+		private static final long serialVersionUID = -6088142603048630416L;
+		
+		private final UII18NService _i18n;
+		
+		@Override
+		public ValidationResult apply(final PersonID value,
+									  final ValueContext context) {
+			if (value == null || Strings.isNullOrEmpty(value.asString())) return ValidationResult.error(_i18n.getMessage("user.login.registry.validation.field"));
+
+			return NIFValidator.isValidNif(value.asString()) ? ValidationResult.ok()
+															 : ValidationResult.error(_i18n.getMessage("user.login.registry.validation.field"));
 		}
 	}
 }

792333c882f272c28c8a71621ae4a9776d5c395b
vanesa.macarron <vmacarro@pge.elkarlan.euskadi.eus>
2021-01-12 13:46:33 +0100

    #1 show usr/pass form as a layout inside the same view

---
+++ r01fVaadin8UI/r01fVaadin8UISecurityClasses/src/main/java/r01ui/ui/vaadin/security/theme/R01UISecurityTheme.java
@@ -0,0 +1,14 @@
+package r01ui.ui.vaadin.security.theme;
+
+import r01f.ui.vaadin.styles.VaadinValoTheme;
+
+public class R01UISecurityTheme 
+	 extends VaadinValoTheme{
+
+	// Login page
+	public static final String LOGIN_PANEL = "login-panel";
+	public static final String LOGIN_TITLE = "login-title";
+	public static final String LOGIN_FIELDS = "fields";
+	public static final String LOGIN_LANG_SELECTOR = "login-lang-selector";
+	
+}

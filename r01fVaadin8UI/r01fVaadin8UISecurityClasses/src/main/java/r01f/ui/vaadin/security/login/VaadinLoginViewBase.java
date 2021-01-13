e12ce02b4edcda74cec6ee7b4cb6efa190b47c00
vanesa.macarron <vmacarro@pge.elkarlan.euskadi.eus>
2021-01-13 17:04:47 +0100

    #1 styles

--- r01fVaadin8UI/r01fVaadin8UISecurityClasses/src/main/java/r01f/ui/vaadin/security/login/VaadinLoginViewBase.java
+++ r01fVaadin8UI/r01fVaadin8UISecurityClasses/src/main/java/r01f/ui/vaadin/security/login/VaadinLoginViewBase.java
@@ -13,18 +13,19 @@ import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinService;
 import com.vaadin.server.WrappedSession;
 import com.vaadin.shared.ui.ContentMode;
+import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Composite;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Window;
 import com.vaadin.ui.themes.ValoTheme;
 
 import lombok.experimental.Accessors;
@@ -48,7 +49,36 @@ import r01f.ui.vaadin.view.VaadinViewI18NMessagesCanBeUpdated;
 import r01f.ui.vaadin.view.VaadinViewID;
 import r01f.util.types.Strings;
 import r01f.util.types.locale.Languages;
+import r01ui.ui.vaadin.security.theme.R01UISecurityTheme;
 
+/**
+ * Ensure to include the [r01uilogin.scss] styles file:
+ * [1] - Go to the UI type and check the @Theme annotation
+ * 		 It should be something like:
+ *		        @Theme("myAppStyles")	
+ *		        public class MyAppVaadinUI
+ *		        	 extends UI {
+ *		        	...
+ *		        }
+ * [2] - There must exist this folder: [WebContent]/VAADIN/themes/myAppStyles  <-- the @Theme annotation value
+ * 		 The folder's contents should be:
+ * 			[WebContent]/VAADIN/themes/myAppStyles
+ * 				+ styles.scss
+ * 				+ addons.scss
+ * 				+ favicon.ico
+ * 				+ myAppStyles.scss  	<-- the @Theme annotation value
+ * 				+ r01uilogin.scss		<-- this file contains the login view styles
+ * [3] - The [styles.scss] file must contain:
+ *			@import "addons.scss";
+ *			@import "myAppStyles.scss";
+ *			@import "r01uilogin.scss";	
+ *
+ *			.myAppStyles {
+ *				@include addons;
+ *				@include myAppStyles;	<-- the @Theme annotation value
+ *				@include r01uilogin;		
+ *			} 
+ */
 
 @Slf4j
 @Accessors(prefix = "_")
@@ -74,10 +104,20 @@ public abstract class VaadinLoginViewBase<U extends User,S extends SecurityConte
 	private final Label _welcome;
 	private final Label _title;
 	private final Label _instructions;
+
+	private final VerticalLayout _buttonsLy;
 	private final Button _googleLoginBtn;
 	private final Button _xlnetsLoginBtn;
 	private final Button _justiziaLoginBtn;
 	private final Button _userPassLink;
+	
+	private Component _usrPassForm;
+	private TextField _txtUser;
+	private TextField _txtPassword;
+	private Button _btnSignIn;
+	private Button _btnBack;
+	private Button _registerBtn;
+	private Button _remindBtn;
 		
 
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -95,10 +135,12 @@ public abstract class VaadinLoginViewBase<U extends User,S extends SecurityConte
 		_welcome = new Label();
 		_title = new Label();
 		_instructions = new Label();
+		_buttonsLy = new VerticalLayout();
 		_googleLoginBtn = new Button();
 		_xlnetsLoginBtn = new Button();
 		_justiziaLoginBtn = new Button();
 		_userPassLink = new Button();
+
 		
 		Component loginForm = _buildLoginForm();
 		updateI18NMessages(_i18n);
@@ -119,6 +161,13 @@ public abstract class VaadinLoginViewBase<U extends User,S extends SecurityConte
 		_xlnetsLoginBtn.setCaption(i18n.getMessage("security.login.method.xlnets"));
 		_justiziaLoginBtn.setCaption(i18n.getMessage("security.login.method.justizia"));
 		_userPassLink.setCaption(i18n.getMessage("security.login.method.userPass"));
+		_txtUser.setCaption(i18n.getMessage("security.login.field.user"));
+		_txtPassword.setCaption(i18n.getMessage("security.login.field.pass"));
+		_btnSignIn.setCaption(i18n.getMessage("security.login.button.enter"));
+		_btnBack.setCaption(i18n.getMessage("security.login.button.back"));
+		_registerBtn.setCaption(i18n.getMessage("security.login.register"));
+		_remindBtn.setCaption(i18n.getMessage("security.login.remind"));
+		
 	}	
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -165,7 +214,7 @@ public abstract class VaadinLoginViewBase<U extends User,S extends SecurityConte
 		loginPanel.setComponentAlignment(header, Alignment.BOTTOM_CENTER);
 		loginPanel.setComponentAlignment(footer, Alignment.TOP_RIGHT);
 		loginPanel.setExpandRatio(header, 1);
-		loginPanel.setExpandRatio(mainPanel, 4);
+		loginPanel.setExpandRatio(mainPanel, 6);
 		loginPanel.setExpandRatio(footer, 1);
 		
 		root.addComponent(loginPanel);
@@ -177,16 +226,16 @@ public abstract class VaadinLoginViewBase<U extends User,S extends SecurityConte
 		_welcome.addStyleNames(ValoTheme.LABEL_H1,
 		 					  ValoTheme.LABEL_BOLD,
 		 					  ValoTheme.LABEL_NO_MARGIN,
-		 					  "login-title");
+		 					  R01UISecurityTheme.LOGIN_TITLE);
 		return _welcome;
 	}
 		
 	private Component _buildMainLayout() {
-		VerticalLayout buttons = new VerticalLayout();
-		buttons.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
-		buttons.addStyleName("login-panel");
-		buttons.setSizeFull();
-		buttons.setMargin(true);
+		VerticalLayout mainLy = new VerticalLayout();
+		mainLy.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		mainLy.addStyleName(R01UISecurityTheme.LOGIN_PANEL);
+		mainLy.setSizeFull();
+		mainLy.setMargin(true);
 		
 		_title.addStyleNames(ValoTheme.LABEL_H2,
 							 ValoTheme.LABEL_BOLD);
@@ -196,158 +245,182 @@ public abstract class VaadinLoginViewBase<U extends User,S extends SecurityConte
 									ValoTheme.LABEL_LIGHT);
 		
 		
-		// Google login
-		String gLoginUrl = Strings.customized("{}?to={}/{}/",		// http://site/app/google/users/login?to=http://localhost:8080/r01PLATEAWebServiceCatalogUIWar/
-											  _securityLoginConfig.getProvider(SecurityProviderID.forId("google")).getLoginUrl(),
-											  _securityLoginConfig.getUrlVars().getProperty("frontEndUrlBase"),
-											  _getWebAppUrlPath());		
-		_googleLoginBtn.setIcon(new ThemeResource("img/google-32x32.svg"));
-		_googleLoginBtn.addStyleNames(ValoTheme.BUTTON_DANGER,
-									"google-button");
-		_googleLoginBtn.addListener(clickEvent ->  Page.getCurrent().open(gLoginUrl, "_blank"));
-		
 		// XLNets login
+		Image xlnetsIcon = new Image();
+		xlnetsIcon.setIcon(new ThemeResource("img/xlnets-32x32.png"));
 		String xlnetsLoginUrl = Strings.customized("{}?to={}/{}/",		// http://site/app/google/users/login?to=http://localhost:8080/r01PLATEAWebServiceCatalogUIWar/
 											   _securityLoginConfig.getProvider(SecurityProviderID.forId("xlnets")).getLoginUrl(),
 											   _securityLoginConfig.getUrlVars().getProperty("frontEndUrlBase"),
 											   _getWebAppUrlPath());
 		_xlnetsLoginBtn.addStyleNames(ValoTheme.BUTTON_PRIMARY, 
 									  "xlnets-button");
-		_xlnetsLoginBtn.setIcon( new ThemeResource("img/xlnets-32x32.png")); //TODO generic url for themes
 		_xlnetsLoginBtn.addListener(clickEvent ->  Page.getCurrent().open(xlnetsLoginUrl, "_blank"));
+		HorizontalLayout xlnetsLy = new HorizontalLayout(xlnetsIcon, _xlnetsLoginBtn);
+		xlnetsLy.setComponentAlignment(xlnetsIcon, Alignment.MIDDLE_RIGHT);
+		xlnetsLy.setComponentAlignment(_xlnetsLoginBtn, Alignment.MIDDLE_LEFT);
+		xlnetsLy.setExpandRatio(xlnetsIcon, 1);
+		xlnetsLy.setExpandRatio(_xlnetsLoginBtn, 3);
+		xlnetsLy.setWidthFull();
+		
+		// Google login
+		Image googleIcon = new Image();
+		googleIcon.setIcon(new ThemeResource("img/google-32x32.svg"));
+		String gLoginUrl = Strings.customized("{}?to={}/{}/",		// http://site/app/google/users/login?to=http://localhost:8080/r01PLATEAWebServiceCatalogUIWar/
+											  _securityLoginConfig.getProvider(SecurityProviderID.forId("google")).getLoginUrl(),
+											  _securityLoginConfig.getUrlVars().getProperty("frontEndUrlBase"),
+											  _getWebAppUrlPath());		
+		_googleLoginBtn.addStyleNames(ValoTheme.BUTTON_DANGER,
+									"google-button");
+		_googleLoginBtn.addListener(clickEvent ->  Page.getCurrent().open(gLoginUrl, "_blank"));
+		HorizontalLayout gLy = new HorizontalLayout(googleIcon, _googleLoginBtn);
+		gLy.setComponentAlignment(googleIcon, Alignment.MIDDLE_RIGHT);
+		gLy.setComponentAlignment(_googleLoginBtn, Alignment.MIDDLE_LEFT);
+		gLy.setExpandRatio(googleIcon, 1);
+		gLy.setExpandRatio(_googleLoginBtn, 3);
+		gLy.setWidthFull();
 		
 		// Justizia login
+		Image jzIcon = new Image();
+		jzIcon.setIcon(new ThemeResource("img/logo_justizia.png"));
 		String justiziaLoginUrl = Strings.customized("{}?to={}/{}/",		// future implementation
 											   "http://justizia-login-url",
 											   _securityLoginConfig.getUrlVars().getProperty("frontEndUrlBase"),
 											   _getWebAppUrlPath());
 		_justiziaLoginBtn.addStyleNames(ValoTheme.BUTTON_PRIMARY, 
 										"justizia-button");
-		_justiziaLoginBtn.setIcon( new ThemeResource("img/logo_justizia.png"));
 		_justiziaLoginBtn.addListener(clickEvent ->  Page.getCurrent().open(justiziaLoginUrl, "_blank"));
+		HorizontalLayout jzLy = new HorizontalLayout(jzIcon, _justiziaLoginBtn);
+		jzLy.setComponentAlignment(jzIcon, Alignment.MIDDLE_RIGHT);
+		jzLy.setComponentAlignment(_justiziaLoginBtn, Alignment.MIDDLE_LEFT);
+		jzLy.setExpandRatio(jzIcon, 1);
+		jzLy.setExpandRatio(_justiziaLoginBtn, 3);
+		jzLy.setWidthFull();
+		jzLy.setVisible(false); //future implementation
+		
+		_buttonsLy.addComponents(xlnetsLy, 
+							   	jzLy, 
+							    gLy);
+		_buttonsLy.setMargin(new MarginInfo(false,true,false,true)); //right & left margin
+		_buttonsLy.setComponentAlignment(xlnetsLy, Alignment.TOP_CENTER);
+		_buttonsLy.setComponentAlignment(jzLy, Alignment.MIDDLE_CENTER); 
+		_buttonsLy.setComponentAlignment(gLy, Alignment.BOTTOM_CENTER);
+		_buttonsLy.setSpacing(true);
+		_usrPassForm = _buildUserPwdFields();
 		
 		// User & password
 		_userPassLink.addStyleName(ValoTheme.BUTTON_LINK);
 		_userPassLink.addClickListener(clickEvent -> {
-													Window dialog = new VaadinLoginUserPasswordWindow();
-													UI.getCurrent().addWindow(dialog);
-		});
+														_buttonsLy.setVisible(false);
+														_userPassLink.setVisible(false);
+														_usrPassForm.setVisible(true);
+													 });
 		_userPassLink.setSizeFull();
 		
+		
 		// Add components
-		buttons.addComponents(_title, 
+		mainLy.addComponents(_title, 
 							  _instructions,
-							  _googleLoginBtn,
-							  _xlnetsLoginBtn, 
-							  //_justiziaLoginBtn, //future implementation
+							  _buttonsLy,
+							  _usrPassForm,
 							  _userPassLink);
+		mainLy.setExpandRatio(_title, 1);
+		mainLy.setExpandRatio(_instructions, 1);
+		mainLy.setExpandRatio(_buttonsLy, 5);
+		mainLy.setExpandRatio(_usrPassForm, 5);
+		mainLy.setExpandRatio(_userPassLink, 1);
 
-		return buttons;
+		return mainLy;
 	}
 
 	private Component _buildLangSelector(final Language... lang) {
-			
+
 		HorizontalLayout langSelector = new HorizontalLayout();
 		langSelector.setMargin(false);
 		langSelector.setDefaultComponentAlignment(Alignment.TOP_RIGHT);
-		langSelector.addStyleName("login-lang-selector");
-		
+		langSelector.addStyleName(R01UISecurityTheme.LOGIN_LANG_SELECTOR);
+
 		FluentIterable.from(lang).forEach(l -> {
-											Button langButton = new Button(Languages.languageLowerCase(l));
-											langButton.addStyleNames(ValoTheme.BUTTON_BORDERLESS,
-																	ValoTheme.LABEL_BOLD);
-											langButton.addClickListener(clickEvent -> {
-																							UI.getCurrent().setLocale(Languages.getLocale(l));
-																							updateI18NMessages(_i18n);
-																					  });
-											langSelector.addComponent(langButton);
-										  });
+			Button langButton = new Button(Languages.languageLowerCase(l));
+			langButton.addStyleNames(ValoTheme.BUTTON_BORDERLESS,
+					ValoTheme.LABEL_BOLD);
+			langButton.addClickListener(clickEvent -> {
+				UI.getCurrent().setLocale(Languages.getLocale(l));
+				updateI18NMessages(_i18n);
+			});
+			langSelector.addComponent(langButton);
+		});
 		return langSelector;
 	}
-	
-/////////////////////////////////////////////////////////////////////////////////////////
-//
-/////////////////////////////////////////////////////////////////////////////////////////		
-	private class VaadinLoginUserPasswordWindow
-	 	  extends Window
-	   implements VaadinViewI18NMessagesCanBeUpdated{
 
-		private static final long serialVersionUID = -3134021582323591011L;
-		
-		protected VaadinLoginUserPasswordWindow() {
-			this.center();
-			this.setModal(true);
-			this.setWidth(35, Unit.PERCENTAGE);
-			this.setResizable(false);
-			Component loginForm = _buildFields();
-			this.setContent(loginForm);
-		
+	private Component _buildUserPwdFields() {
+		VerticalLayout usrPassFields = new VerticalLayout();
+		usrPassFields.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		usrPassFields.setMargin(false);
+		usrPassFields.setSpacing(true);
+		usrPassFields.addStyleName(R01UISecurityTheme.LOGIN_FIELDS);
+		usrPassFields.setVisible(false);
+
+		// User password
+		_txtUser = new TextField();
+		_txtUser.setIcon(VaadinIcons.USER);
+		_txtUser.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
+
+		_txtPassword = new PasswordField();
+		_txtPassword.setIcon(VaadinIcons.LOCK);
+		_txtPassword.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
+
+
+		_btnSignIn = new Button();
+		_btnSignIn.addStyleName(ValoTheme.BUTTON_PRIMARY);
+		_btnSignIn.setClickShortcut(KeyCode.ENTER);
+		_btnSignIn.addClickListener(clickEvent -> {
+													// Call the login
+													_userPasswordLogin(LoginID.forId(_txtUser.getValue()),
+																	   Password.forId(_txtPassword.getValue()));
+												 });
+		_btnBack = new Button();
+		_btnBack.addClickListener(clickEvent -> {
+													_buttonsLy.setVisible(true);
+													_userPassLink.setVisible(true);
+													_usrPassForm.setVisible(false);
+											   });
+		HorizontalLayout buttonsLy = new HorizontalLayout(_btnBack, 
+														  _btnSignIn);
+		buttonsLy.setMargin(false);
+
+		_registerBtn = new Button();
+		_registerBtn.addStyleName(ValoTheme.BUTTON_LINK);
+		_remindBtn = new Button();
+		_remindBtn.addStyleName(ValoTheme.BUTTON_LINK);
+
+		// Add components
+		usrPassFields.addComponents(_txtUser,
+									_txtPassword,
+									buttonsLy,
+									_registerBtn,
+									_remindBtn);
+		_btnSignIn.focus();
+
+		return usrPassFields;
+	}
+
+	private void _userPasswordLogin(final LoginID login,
+			final Password password) {
+		// use the presenter to do the login
+		LoginSession<U> loginSession = _presenter.userPasswordLogin(login,
+				password);
+		if (loginSession != null) {
+			// goto the main page
+			Url mainPageUrl = Url.from(Strings.customized("/{}#!{}",
+					_getWebAppUrlPath(),_getMainViewId()));
+			log.warn("[login] > user {} has logged-in > redir to MAIN page: {}.....................................",
+					loginSession.getLoginId(),
+					mainPageUrl);
+			Page.getCurrent().setLocation(mainPageUrl.asString());
+		} else {
+			Notification.show(_i18n.getMessage("security.login.invalid.user.or.passwd"),
+							  Notification.Type.ERROR_MESSAGE);
 		}
-		
-		private Component _buildFields() {
-			VerticalLayout mainLayout = new VerticalLayout();
-			mainLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
-			mainLayout.setMargin(true);
-			mainLayout.setSpacing(true);
-			mainLayout.addStyleName("fields");
-			
-			// User password
-			TextField txtUser = new TextField(_i18n.getMessage("security.login.field.user"));
-			txtUser.setIcon(VaadinIcons.USER);
-			txtUser.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
-			
-			PasswordField txtPassword = new PasswordField(_i18n.getMessage("security.login.field.pass"));
-			txtPassword.setIcon(VaadinIcons.LOCK);
-			txtPassword.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
-			
-			
-			Button btnSignIn = new Button(_i18n.getMessage("security.login.button.enter"));
-			btnSignIn.addStyleName(ValoTheme.BUTTON_PRIMARY);
-			btnSignIn.setClickShortcut(KeyCode.ENTER);
-			btnSignIn.addClickListener(clickEvent -> {
-											// Call the login
-											_userPasswordLogin(LoginID.forId(txtUser.getValue()),
-															   Password.forId(txtPassword.getValue()));
-									   });
-			
-			Button registerBtn = new Button(_i18n.getMessage("security.login.register"));
-			registerBtn.addStyleName(ValoTheme.BUTTON_LINK);
-			//registerBtn.setSizeFull();
-		
-			Button remindBtn = new Button(_i18n.getMessage("security.login.remind"));
-			remindBtn.addStyleName(ValoTheme.BUTTON_LINK);
-			//remindBtn.setSizeFull();
-			
-			
-			// Add components
-			mainLayout.addComponents(txtUser,
-								 	 txtPassword,
-								 	 btnSignIn,
-								 	 registerBtn,
-								 	 remindBtn);
-			btnSignIn.focus();
-	
-			return mainLayout;
-		}
-		
-		private void _userPasswordLogin(final LoginID login,final Password password) {
-			// use the presenter to do the login
-			LoginSession<U> loginSession = _presenter.userPasswordLogin(login,
-											 			    			password);
-			if (loginSession != null) {
-				// goto the main page
-				Url mainPageUrl = Url.from(Strings.customized("/{}#!{}",
-															  _getWebAppUrlPath(),_getMainViewId()));
-				log.warn("[login] > user {} has logged-in > redir to MAIN page: {}.....................................",
-						 loginSession.getLoginId(),
-						 mainPageUrl);
-				Page.getCurrent().setLocation(mainPageUrl.asString());
-			} else {
-				Notification.show(_i18n.getMessage("security.login.invalid.user.or.passwd"),
-								  Notification.Type.ERROR_MESSAGE);
-			}
-		}
-		
 	}
 	
 /////////////////////////////////////////////////////////////////////////////////////////

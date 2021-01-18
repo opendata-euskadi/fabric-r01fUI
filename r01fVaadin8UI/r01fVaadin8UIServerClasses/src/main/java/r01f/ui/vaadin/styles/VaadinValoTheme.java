7cddd79bda83802614070dea07ad83a6fbc12cab
ateminov <ateminov@CO02338E2>
2021-01-18 10:40:14 +0100

    Create r01fuicommon "theme" and comment unused constants

--- r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/styles/VaadinValoTheme.java
+++ r01fVaadin8UI/r01fVaadin8UIServerClasses/src/main/java/r01f/ui/vaadin/styles/VaadinValoTheme.java
@@ -10,11 +10,8 @@ public abstract class VaadinValoTheme
 			  extends ValoTheme {
 /////////////////////////////////////////////////////////////////////////////////////////
 // 	STYLES CONSTANTS
-/////////////////////////////////////////////////////////////////////////////////////////
-	public static final String CUSTOM_WINDOW = "r01ui-window";
+/////////////////////////////////////////////////////////////////////////////////////////	
 	public static final String CREATE_EDIT_WINDOW = "r01ui-create-edit-detail-view";
-	
-	public static final String HIDE = "hide";
 	
 	public static final String NO_PADDING = "no-padding";
 	public static final String NO_PADDING_TOP = "no-padding-top";
@@ -22,81 +19,84 @@ public abstract class VaadinValoTheme
 	public static final String NO_MARGIN_TOP = "no-margin-top";
 	public static final String NO_MARGIN_BOTTOM = "no-margin-bottom";
 	
-	public static final String PADDING_TOP = "padding-top";
-	public static final String PADDING_BOTTOM = "padding-bottom";
+	//public static final String WIDTH_AUTO = "width-auto";	
+	//public static final String NON_SELECT = "nonselect";
+	//public static final String NON_SELECT_ALL = "nonselect-all";
 	
-	public static final String WIDTH_AUTO = "width-auto";	
-	
-	public static final String NON_SELECT = "nonselect";
-	public static final String NON_SELECT_ALL = "nonselect-all";
-
-	public static final String TEXT_FIELD_UPPERCASE = "upper-case";
 	public static final String COMBO_SMALL_SIZE = "combo-small";
 	public static final String COMBO_MEDIUM_SIZE = "combo-medium";
 	public static final String COMBO_BIG_SIZE = "combo-big";
 	public static final String INPUT_SMALL_SIZE = "input-small";
 	public static final String INPUT_MEDIUM_SIZE = "input-medium";
 	public static final String INPUT_BIG_SIZE = "input-big";
-	public static final String INPUT_HUGE_SIZE = "input-huge";
-	public static final String TEXTAREA_SMALL_SIZE = "textarea-small";
-	public static final String TEXTAREA_MEDIUM_SIZE = "textarea-medium";
-	public static final String TEXTAREA_BIG_SIZE = "textarea-big";
-	public static final String DATEFIELD_SMALL_SIZE = "datefield-small";
-	public static final String DATEFIELD_MEDIUM_SIZE = "datefield-medium";
-	public static final String DATEFIELD_BIG_SIZE = "datefield-big";
+	public static final String INPUT_HUGE_SIZE = "input-huge";	
+//	public static final String TEXTAREA_SMALL_SIZE = "textarea-small";
+//	public static final String TEXTAREA_MEDIUM_SIZE = "textarea-medium";
+//	public static final String TEXTAREA_BIG_SIZE = "textarea-big";
+//	public static final String DATEFIELD_SMALL_SIZE = "datefield-small";
+//	public static final String DATEFIELD_MEDIUM_SIZE = "datefield-medium";
+//	public static final String DATEFIELD_BIG_SIZE = "datefield-big";
 	
 	public static final String LABEL_AND_ADD_BUTTON = "lbl-add-button";
-	public static final String LABEL_WITH_ICON = "label-with-icon";
-	
-	
-	public static final String GRID_NO_CELL_HEADERS = "r01ui-grid-no-cell-headers";
+	//public static final String LABEL_WITH_ICON = "label-with-icon";
+	//public static final String GRID_NO_CELL_HEADERS = "r01ui-grid-no-cell-headers";
 	public static final String GRID_COMPONENT = "r01ui-object-grid-component";
 	public static final String GRID_STRIPPED = "stripes";
-	public static final String GRID_NO_BORDER = "r01ui-grid-no-border";
+	//public static final String GRID_NO_BORDER = "r01ui-grid-no-border";
 	public static final String GRID_CELL_NO_BORDER = "r01ui-grid-cell-no-border";
-	public static final String GRID_CONTAINER_SMALL_SIZE = "grid-container-small";
-	public static final String GRID_CONTAINER_MEDIUM_SIZE = "grid-container-medium";
-	public static final String GRID_CONTAINER_BIG_SIZE = "grid-container-big";
+//	public static final String GRID_CONTAINER_SMALL_SIZE = "grid-container-small";
+//	public static final String GRID_CONTAINER_MEDIUM_SIZE = "grid-container-medium";
+//	public static final String GRID_CONTAINER_BIG_SIZE = "grid-container-big";
 	public static final String GRID_NO_RESULTS_LABEL = "grid-lbl-no-results";
 	public static final String GRID_DOTS_MENU = "grid-dots-menu";
+	
 	public static final String LAYOUT_WHITE_BORDERED = "r01ui-white-bordered-layout";
+	public static final String VIEW_CONTAINER = "view-container";
+	
+	public static final String BUTTON_ADD = "add-button";
+	public static final String CHECKBOX_DEFAULT = "checkbox-default";
+	
+	public static final String MENU_HIDE = "hide";
+	public static final String MENU_ITEM_CHECKED = "checked";	
+	
+/////////////////////////////////////////////////////////////////////////////////////////
+//	ONLY USE IN STRUCTURE APP
+/////////////////////////////////////////////////////////////////////////////////////////
+	public static final String TOOLBAR = "toolbar";
+	public static final String CUSTOM_WINDOW = "r01ui-window";
+	
+/////////////////////////////////////////////////////////////////////////////////////////
+//	ONLY USE IN DIRECTORY APP
+/////////////////////////////////////////////////////////////////////////////////////////
+	public static final String HIDE = "hide";
+		
+	public static final String PADDING_TOP = "padding-top";
+	public static final String PADDING_BOTTOM = "padding-bottom";	
+	public static final String TEXT_FIELD_UPPERCASE = "upper-case";
 	
 	public static final String MAIN_MENU_BAR = "main-menu-bar";
 	public static final String SIDE_BAR = "sidebar";
 	public static final String EDIT_VIEW = "edit-view";
-	public static final String VIEW_CONTAINER = "view-container";
-	public static final String CONTAINER_BASE = "container-base";
 	
-	public static final String TABSHEET = "tabsheet";
-	
-	public static final String BUTTON_ADD = "add-button";
 	public static final String PRIMARY_BUTTON = "primary-button";
 	public static final String SECONDARY_BUTTON = "secondary-button";
-
-	
-	public static final String CHECKBOX_DEFAULT = "checkbox-default";
 	
 	public static final String OPTION_GROUP_INLINE = "option-group-inline";
 	
-	public static final String MENU_HIDE = "hide";
-	public static final String MENU_ITEM_CHECKED = "checked";
-
-	public static final String SECTION = "section";
-	public static final String SELECTED = "selected";
-	public static final String EDIT_LAYOUT = "edit-layout";
-	
-	public static final String TOOLBAR_ACTIONS_MENU = "toolbar-actions-menu";
-	public static final String TOOLBAR = "toolbar";
-	
 	public static final String CHECK_WITH_ICON = "check-with-icon";
+
+	//public static final String CONTAINER_BASE = "container-base";
+	//public static final String TABSHEET = "tabsheet";	
 	
-	public static final String DANGER_COLOR = "danger-color";
-	public static final String SAVE_COLOR = "save-color";
+	//public static final String SECTION = "section";
+	//public static final String SELECTED = "selected";
+	//public static final String EDIT_LAYOUT = "edit-layout";
+	//public static final String TOOLBAR_ACTIONS_MENU = "toolbar-actions-menu";	
 	
-	public static final String READ_ONLY = "read-only";
-	
-	public static final String ITEM = "item";
-	
-	public static final String WRAP_LINE = "wrapline";
-	public static final String JUSTIFY = "justify";
+	//public static final String DANGER_COLOR = "danger-color";
+	//public static final String SAVE_COLOR = "save-color";
+	//public static final String READ_ONLY = "read-only";
+	//public static final String ITEM = "item";	
+	//public static final String WRAP_LINE = "wrapline";
+	//public static final String JUSTIFY = "justify";
 }

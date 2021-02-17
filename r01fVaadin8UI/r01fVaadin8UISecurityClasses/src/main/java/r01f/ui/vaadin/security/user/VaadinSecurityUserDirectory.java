109e61227af90b938b6f1b0546fa62cf2d5d90fe
pci <pci@ejie.eus>
2021-02-17 19:04:00 +0100

    VaadinSecurityUserDirectory > add the SecurityProviderID to allow multiple corporate directories in the future

--- r01fVaadin8UI/r01fVaadin8UISecurityClasses/src/main/java/r01f/ui/vaadin/security/user/VaadinSecurityUserDirectory.java
+++ r01fVaadin8UI/r01fVaadin8UISecurityClasses/src/main/java/r01f/ui/vaadin/security/user/VaadinSecurityUserDirectory.java
@@ -3,15 +3,20 @@ package r01f.ui.vaadin.security.user;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import r01f.locale.I18NKey;
+import r01f.securitycontext.SecurityIDS.SecurityProviderID;
 import r01f.ui.i18n.UII18NService;
 
 @RequiredArgsConstructor(access=AccessLevel.PRIVATE)
 public enum VaadinSecurityUserDirectory {
-	LOCAL		(I18NKey.named("security.directory.local")),
-	CORPORATE	(I18NKey.named("security.directory.corporate"));
+	LOCAL		(null,I18NKey.named("security.directory.local")),
+	CORPORATE	(SecurityProviderID.XLNETS,I18NKey.named("security.directory.corporate"));
 
+	private final SecurityProviderID _providerId;
 	private final I18NKey _i18nKey;
 
+	public SecurityProviderID getProviderId() {
+		return _providerId;
+	}
 	public String getNameUsing(final UII18NService i18n) {
 		return i18n.getMessage(_i18nKey);
 	}
